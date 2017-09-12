87fa1a395100e8803d2265701ebc76d383fdb358
aevitas <m4rtijn@gmail.com>
2017-09-12 20:57:01 +0200

    Actually inject the request delegate into the middleware

--- src/Hotline.Core/HotlineMiddleware.cs
+++ src/Hotline.Core/HotlineMiddleware.cs
@@ -15,10 +15,13 @@ namespace Hotline.Core
         /// <summary>
         ///     Initializes a new instance of the <see cref="HotlineMiddleware" /> class.
         /// </summary>
-        /// <param name="hotline">The hotline.</param>
-        public HotlineMiddleware(IHotline hotline)
+        /// <param name="hotline">The hotline we'll be calling to capture exceptions.</param>
+        /// <param name="request">The request.</param>
+        /// <exception cref="ArgumentNullException">hotline</exception>
+        public HotlineMiddleware(IHotline hotline, RequestDelegate request)
         {
             _hotline = hotline ?? throw new ArgumentNullException(nameof(hotline));
+            _request = request ?? throw new ArgumentNullException(nameof(request));
         }
 
         public async Task Invoke(HttpContext context)
